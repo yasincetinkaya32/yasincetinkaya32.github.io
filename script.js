5e5bb957fac07bfaa5420f04d2c9cb7554c54531
Yasin Çetinkaya <yasincetinkaya022@gmail.com>
2026-02-21 21:41:05 +0300

    Modify openTab function to accept button parameter

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function openTab(tabId) {
+function openTab(tabId, btn) {
   document.querySelectorAll(".tab-content").forEach(tab => {
     tab.classList.remove("active");
   });
 
-  document.querySelectorAll(".tab-btn").forEach(btn => {
-    btn.classList.remove("active");
+  document.querySelectorAll(".tab-btn").forEach(button => {
+    button.classList.remove("active");
   });
 
   document.getElementById(tabId).classList.add("active");
-  event.target.classList.add("active");
+  btn.classList.add("active");
 }
